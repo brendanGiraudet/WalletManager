160c571a8c5fd5ab9022e5bcfcd3b453040fe2d7
baka <bakayarusama@gmail.com>
2020-04-03 18:53:25 +0200

    Add service and dto library

---
+++ Sources/WalletManagerDTO/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WalletManagerDTO
+{
+    public class Class1
+    {
+    }
+}
---
+++ Sources/WalletManagerServices/Transaction/TransactionServices.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace WalletManagerServices
+{
+    public class TransactionServices
+    {
+    }
+}
