edec9e5c6e2a910a652c8488d77fc43c490ae808
baka <bakayarusama@gmail.com>
2020-04-03 19:58:39 +0200

    Rename file and add transaction dto

--- Sources/WalletManagerDTO/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace WalletManagerDTO
-{
-    public class Class1
-    {
-    }
-}
---
+++ Sources/WalletManagerDTO/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WalletManagerDTO
+{
+    public class Transaction
+    {
+        public string Compte { get; set; }
+
+        public DateTime ComptabilisationDate { get; set; }
+
+        public DateTime OperationDate { get; set; }
+
+        public string Label { get; set; }
+
+        public string Reference { get; set; }
+
+        public DateTime ValueDate { get; set; }
+
+        public double Amount { get; set; }
+    }
+}
