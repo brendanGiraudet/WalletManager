43499acc7899505b43a1c763acf14d262df28d76
baka <bakayarusama@gmail.com>
2020-04-04 01:18:33 +0200

    Add transaction controller and viewmodel with index page

---
+++ Sources/WalletManagerSite/Controllers/TransactionController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WalletManagerServices.Serializer;
+
+namespace WalletManagerSite.Controllers
+{
+    public class TransactionController : Controller
+    {
+        readonly ISerializer _transactionSerializer;
+        public TransactionController(ISerializer transactionSerializer)
+        {
+            _transactionSerializer = transactionSerializer;
+        }
+        // GET: Transaction
+        public ActionResult Index()
+        {
+            return View(GetTransactions());
+        }
+
+        private List<Models.TransactionViewModel> GetTransactions()
+        {
+            var csvPath = @"D:\document\project\WalletManager\Sources\WalletManagerTestProject\CSV\serialize.csv";
+            var transactions = _transactionSerializer.Deserialize(csvPath);
+            if(transactions != null && transactions.Any())
+            {
+                return transactions.Select(transaction => new Models.TransactionViewModel
+                {
+                    Amount = transaction.Amount,
+                    ComptabilisationDate = transaction.ComptabilisationDate,
+                    Compte = transaction.Compte,
+                    Label = transaction.Label,
+                    OperationDate = transaction.OperationDate,
+                    Reference = transaction.Reference,
+                    ValueDate = transaction.ValueDate
+                }).ToList();
+            }
+            return new List<Models.TransactionViewModel>();
+        }
+
+        [HttpGet]
+        public JsonResult TransactionChart()
+        {
+            var csvPath = @"D:\document\project\WalletManager\Sources\WalletManagerTestProject\CSV\serialize.csv";
+            var transactions = _transactionSerializer.Deserialize(csvPath);
+            
+            return Json(transactions);
+        }
+
+        // GET: Transaction/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Transaction/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Transaction/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Transaction/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Transaction/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Transaction/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Transaction/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Sources/WalletManagerSite/Models/TransactionViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WalletManagerSite.Models
+{
+    public class TransactionViewModel
+    {
+        public string Compte { get; set; }
+
+        public DateTime ComptabilisationDate { get; set; }
+
+        public DateTime OperationDate { get; set; }
+
+        public string Label { get; set; }
+
+        public string Reference { get; set; }
+
+        public DateTime ValueDate { get; set; }
+
+        public double Amount { get; set; }
+    }
+}
