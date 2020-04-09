1125037ac329454b5a6cfceaee1e7a109cbff389
baka <bakayarusama@gmail.com>
2020-04-09 21:31:17 +0200

    add transaction category enum

---
+++ Sources/WalletManagerDTO/Enumerations/TransactionCategory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WalletManagerDTO.Enumerations
+{
+    public enum TransactionCategory
+    {
+        Courses,
+        Voiture,
+        Sante,
+        MalBouffe,
+        Loyer,
+        Taxe,
+        Internet,
+        Telephone,
+        electrecite,
+        gaz
+    }
+}
