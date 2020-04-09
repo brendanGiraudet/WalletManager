2895abbe28ed33408c1d15b2734553c3d32800a5
baka <bakayarusama@gmail.com>
2020-04-09 22:18:40 +0200

    Add implementation of GetTransaction

--- Sources/WalletManagerServices/Transaction/TransactionServices.cs
+++ Sources/WalletManagerServices/Transaction/TransactionServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Configuration;
+using System;
 using System.Collections.Generic;
 
 namespace WalletManagerServices.Transaction
@@ -6,15 +7,25 @@ namespace WalletManagerServices.Transaction
     public class TransactionServices : ITransactionServices
     {
         readonly Serializer.ISerializer _transactionSerializer;
+        readonly IConfigurator _configurator;
 
-        public TransactionServices(Serializer.ISerializer transactionSerializer)
+        public TransactionServices(Serializer.ISerializer transactionSerializer, IConfigurator configurator)
         {
             _transactionSerializer = transactionSerializer;
+            _configurator = configurator;
+        }
+
+        public WalletManagerDTO.Transaction GetTransaction(string reference)
+        {
+            var csvPath = _configurator.GetCsvPath();
+            var transactions = _transactionSerializer.Deserialize(csvPath);
+
+            return transactions.Find(transaction => transaction.Reference.Equals(reference));
         }
 
         public List<WalletManagerDTO.Transaction> GetTransactions()
         {
-            var csvPath = @"D:\document\project\WalletManager\Sources\WalletManagerTestProject\CSV\serialize.csv";
+            var csvPath = _configurator.GetCsvPath();
             return _transactionSerializer.Deserialize(csvPath);
         }
     }
