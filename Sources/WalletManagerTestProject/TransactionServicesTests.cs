a06a18cac156ed3181e105371f34f7f736e990c4
baka <bakayarusama@gmail.com>
2020-04-09 22:19:02 +0200

    Add test method for GetTransaction method

--- Sources/WalletManagerTestProject/TransactionServicesTests.cs
+++ Sources/WalletManagerTestProject/TransactionServicesTests.cs
@@ -1,3 +1,4 @@
+using Configuration;
 using System.Linq;
 using Xunit;
 
@@ -9,14 +10,33 @@ namespace WalletManagerTestProject
         public void ShouldHaveListOfTransactionWhenIGetTransactions()
         {
             // Arrange
+            var configurator = new Configurator();
             var transactionSerializer = new WalletManagerServices.Serializer.TransactionSerializer();
-            var transactionServices = new WalletManagerServices.Transaction.TransactionServices(transactionSerializer);
+            var transactionServices = new WalletManagerServices.Transaction.TransactionServices(transactionSerializer, configurator);
 
             // Act
             var transactions = transactionServices.GetTransactions();
 
             // Assert
             Assert.True(transactions.Any());
+        }
+
+        [Fact]
+        public void ShouldHaveAtransactionWhenIPutAGoodReference()
+        {
+            // Arrange
+            var configurator = new Configurator();
+            var transactionSerializer = new WalletManagerServices.Serializer.TransactionSerializer();
+            var transactionServices = new WalletManagerServices.Transaction.TransactionServices(transactionSerializer, configurator);
+            const string expectedReference = "2V7926X";
+
+            // Act
+            var transaction = transactionServices.GetTransaction(expectedReference);
+
+            // Assert
+            Assert.NotNull(transaction);
+            Assert.Equal(transaction.Reference, expectedReference);
+
         }
     }
 }
