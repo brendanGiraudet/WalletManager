005dd50d29dec6e23014bf150d427d645943dfd3
baka <bakayarusama@gmail.com>
2020-04-18 17:03:58 +0200

    Listing of csv files feature

---
+++ Sources/WalletManagerSite/Controllers/CompareController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using WalletManagerServices.Transaction;
+using WalletManagerSite.Models;
+
+namespace WalletManagerSite.Controllers
+{
+    public class CompareController : Controller
+    {
+        readonly IConfiguration _configuration;
+
+        public CompareController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        // GET: Compare
+        public ActionResult Index()
+        {
+            return View(GetCsvList());
+        }
+
+        private List<CsvFileViewModel> GetCsvList()
+        {
+            var directoryPath = GetCsvDirectoryPath();
+            var csvFilesName = Directory.GetFiles(directoryPath, "*.csv");
+
+            if(csvFilesName != null && csvFilesName.Any())
+            {
+                var csvFiles = new List<CsvFileViewModel>();
+                foreach (var fileName in csvFilesName)
+                {
+                    var fullPath = Path.Combine(directoryPath, fileName);
+                    var fileInfo = new FileInfo(fullPath);
+                    csvFiles.Add(new CsvFileViewModel
+                    {
+                        CreatedDate = fileInfo.CreationTime,
+                        FileName = fileInfo.Name,
+                        FullPath = fileInfo.FullName,
+                        UpdateDate = fileInfo.LastWriteTime
+                    });
+                }
+                return csvFiles;
+            }
+            return new List<CsvFileViewModel>();
+        }
+
+        private string GetCsvDirectoryPath()
+        {
+            var directoryName = _configuration.GetValue<string>("CsvDirectoryName");
+            return Path.Combine(Directory.GetCurrentDirectory(), directoryName);
+        }
+    }
+}
---
+++ Sources/WalletManagerSite/Models/CsvFileViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WalletManagerSite.Models
+{
+    public class CsvFileViewModel
+    {
+        public string FullPath { get; set; }
+
+        public string FileName { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdateDate { get; set; }
+    }
+}
