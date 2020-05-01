162a690f411018471b32552890e11882f848ac6d
baka <bakayarusama@gmail.com>
2020-05-01 11:48:16 +0200

    Remove unecessary using

--- Sources/WalletManagerSite/Controllers/CompareController.cs
+++ Sources/WalletManagerSite/Controllers/CompareController.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using WalletManagerDTO;
