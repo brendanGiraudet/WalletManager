b3c3e1d20a8f7d2e736307d1443f68b842a04377
baka <bakayarusama@gmail.com>
2020-05-04 22:53:20 +0200

    Add BanquePostaleCsvSerializer + tests

---
+++ Sources/WalletManagerDAL/Serializer/BanquePostaleCsvSerializer.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using WalletManagerDTO;
+
+namespace WalletManagerDAL.Serializer
+{
+    public class BanquePostaleCsvSerializer : ISerializer
+    {
+        public List<Transaction> Deserialize(string csvPath)
+        {
+            IEnumerable<string> csvLines;
+            try
+            {
+                csvLines = File.ReadAllLines(csvPath).Skip(8);
+            }
+            catch (Exception ex)
+            {
+                throw new WalletManagerDTO.Exceptions.SerializerException($"Wrong csv path : {csvPath}", ex);
+            }
+            return Deserialize(csvLines);
+        }
+
+        public List<Transaction> Deserialize(Stream stream)
+        {
+            if (stream == null) return new List<Transaction>();
+
+            var streamReader = new StreamReader(stream);
+            var contentFile = streamReader.ReadToEnd();
+            var csvLines = contentFile.Trim().Split("\n").Skip(8);
+            return Deserialize(csvLines);
+        }
+
+        public List<Transaction> Deserialize(IEnumerable<string> csvLines)
+        {
+            var transactions = new List<Transaction>();
+
+            try
+            {
+                foreach (var csvLine in csvLines)
+                {
+                    var values = csvLine.Split(';').ToArray();
+                    var transaction = new Transaction
+                    {
+                        OperationDate = Convert.ToDateTime(values[0]),
+                        Label = values[1],
+                        Amount = Convert.ToDouble(values[2].Replace('.', ',')),
+                        Category = GetCategory(values)
+                    };
+
+                    transactions.Add(transaction);
+                }
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                throw new WalletManagerDTO.Exceptions.SerializerException($"Wrong csv format", ex);
+            }
+            catch (Exception ex)
+            {
+                throw new WalletManagerDTO.Exceptions.SerializerException($"Error : impossible to deserialize the csv file due to " + ex.Message, ex);
+            }
+
+            return transactions;
+        }
+
+        private static WalletManagerDTO.Enumerations.TransactionCategory GetCategory(string[] values)
+        {
+            try
+            {
+                Enum.TryParse(values.GetValue(4).ToString(), out WalletManagerDTO.Enumerations.TransactionCategory category);
+
+                return category;
+            }
+            catch (Exception)
+            {
+                return WalletManagerDTO.Enumerations.TransactionCategory.NA;
+            }
+        }
+
+        public void Serialize(List<Transaction> transactions, string csvPath)
+        {
+            if (transactions == null || !transactions.Any()) throw new WalletManagerDTO.Exceptions.SerializerException("Impossible to serialize an empty transaction list");
+
+            StringWriter stringWriter = new StringWriter();
+            stringWriter.Write(@"Numéro Compte   ;5380245H033
+Type         ;CCP
+Compte tenu en  ;euros
+Date            ;03/05/2020
+Solde (EUROS)   ;154,22
+Solde (FRANCS)  ;1011,62
+
+Date;Libellé;Montant(EUROS);Montant(FRANCS)");
+            stringWriter.Write(stringWriter.NewLine);
+            var delimiter = ";";
+
+            foreach (var transaction in transactions)
+            {
+                stringWriter.Write(transaction.OperationDate + delimiter);
+                stringWriter.Write(transaction.Label + delimiter);
+                stringWriter.Write(transaction.Amount + delimiter);
+                stringWriter.Write(" " + delimiter);
+                stringWriter.Write(transaction.Category + delimiter);
+                stringWriter.Write(stringWriter.NewLine);
+            }
+
+            try
+            {
+                File.WriteAllText(csvPath, stringWriter.ToString());
+            }
+            catch (Exception ex)
+            {
+                throw new WalletManagerDTO.Exceptions.SerializerException($"Error : impossible to serialize into csv file in path { csvPath } due to " + ex.Message, ex);
+            }
+        }
+    }
+}
---
+++ Sources/WalletManagerTestProject/BanquePostaleCsvSerializerTests.cs
@@ -0,0 +1,154 @@
+﻿using System.Linq;
+using WalletManagerDAL.Serializer;
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.IO;
+
+namespace WalletManagerTestProject
+{
+    public class BanquePostaleCsvSerializerTests
+    {
+        const string banquePostaleCsvBasePath = @"D:\document\project\WalletManager\Sources\WalletManagerTestProject\CSV_Banque_postale\";
+
+        [Theory]
+        [InlineData(banquePostaleCsvBasePath + "deserialize.csv")]
+        [InlineData(banquePostaleCsvBasePath + "deserializeWithCategory.csv")]
+        public void ShouldHaveListOfTransactionWhenIDeserializeACsvString(string csvPath)
+        {
+            // Arrange
+            ISerializer serializer = new BanquePostaleCsvSerializer();
+
+            // Act
+            var transactionList = serializer.Deserialize(csvPath);
+
+            // Assert
+            Assert.True(transactionList.Any());
+        }
+
+        [Theory]
+        [InlineData(banquePostaleCsvBasePath + "deserialize.csv")]
+        [InlineData(banquePostaleCsvBasePath + "deserializeWithCategory.csv")]
+        public void ShouldHaveListOfTransactionWhenIDeserializeAstream(string csvPath)
+        {
+            // Arrange
+            ISerializer serializer = new BanquePostaleCsvSerializer();
+            var expectedStream = File.OpenRead(csvPath);
+
+            // Act
+            var transactionList = serializer.Deserialize(expectedStream);
+
+            // Assert
+            Assert.True(transactionList.Any());
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(banquePostaleCsvBasePath + "wrong.csv")]
+        public void ShouldThrowExceptionWhenIDeserializeAWrongCsvFile(string csvPath)
+        {
+            // Arrange
+            ISerializer serializer = new BanquePostaleCsvSerializer();
+
+            // Act
+            Action deserializeAction = () => serializer.Deserialize(csvPath);
+
+            // Assert
+            Assert.Throws<WalletManagerDTO.Exceptions.SerializerException>(deserializeAction);
+        }
+
+        [Fact]
+        public void ShouldHaveACsvFileWhenISerializeTransactionList()
+        {
+            // Arrange
+            ISerializer serializer = new BanquePostaleCsvSerializer();
+            var csvPath = banquePostaleCsvBasePath + "serialize.csv";
+            var transactions = new List<WalletManagerDTO.Transaction>
+            {
+                new WalletManagerDTO.Transaction
+                {
+                    Compte = "31419185918",
+                    ComptabilisationDate = new DateTime(2020,03,31),
+                    OperationDate = new DateTime(2020,03,31),
+                    Label = "300320 CB****1526 ALLDEBRID.COM  92MONTROUGE",
+                    Reference = "475QGS0",
+                    ValueDate = new DateTime(2020,03,31),
+                    Amount = -15.99,
+                    Category = WalletManagerDTO.Enumerations.TransactionCategory.Internet
+                },
+                new WalletManagerDTO.Transaction
+                {
+                    Compte = "31419185918",
+                    ComptabilisationDate = new DateTime(2020,03,31),
+                    OperationDate = new DateTime(2020,03,31),
+                    Label = "VIR M BRENDAN GIRAUDET Virement vers BRENDAN GIRAUDET",
+                    Reference = "6681107",
+                    ValueDate = new DateTime(2020,03,31),
+                    Amount = -1000,
+                    Category = WalletManagerDTO.Enumerations.TransactionCategory.Courses
+                }
+            };
+
+            // Act
+            serializer.Serialize(transactions, csvPath);
+
+            // Assert
+            Assert.True(true);
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData("Z://bad/csv/path/file")]
+        public void ShouldThrowExceptionWhenISerializeWithWrongCsvPath(string csvPath)
+        {
+            // Arrange
+            ISerializer serializer = new BanquePostaleCsvSerializer();
+            var transactions = new List<WalletManagerDTO.Transaction>
+            {
+                new WalletManagerDTO.Transaction
+                {
+                    Compte = "31419185918",
+                    ComptabilisationDate = new DateTime(2020,03,31),
+                    OperationDate = new DateTime(2020,03,31),
+                    Label = "300320 CB****1526 ALLDEBRID.COM  92MONTROUGE",
+                    Reference = "475QGS0",
+                    ValueDate = new DateTime(2020,03,31),
+                    Amount = -15.99,
+                    Category = WalletManagerDTO.Enumerations.TransactionCategory.Internet
+                },
+                new WalletManagerDTO.Transaction
+                {
+                    Compte = "31419185918",
+                    ComptabilisationDate = new DateTime(2020,03,31),
+                    OperationDate = new DateTime(2020,03,31),
+                    Label = "VIR M BRENDAN GIRAUDET Virement vers BRENDAN GIRAUDET",
+                    Reference = "6681107",
+                    ValueDate = new DateTime(2020,03,31),
+                    Amount = -1000,
+                    Category = WalletManagerDTO.Enumerations.TransactionCategory.Courses
+                }
+            };
+
+            // Act
+            Action serializeAction = () => serializer.Serialize(transactions, csvPath);
+
+            // Assert
+            Assert.Throws<WalletManagerDTO.Exceptions.SerializerException>(serializeAction);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionWhenISerializeEmptyTransactionList()
+        {
+            // Arrange
+            ISerializer serializer = new BanquePostaleCsvSerializer();
+            var emptyTransactionList = new List<WalletManagerDTO.Transaction>();
+            var csvPath = banquePostaleCsvBasePath + "serialize.csv";
+
+            // Act
+            Action serializeAction = () => serializer.Serialize(emptyTransactionList, csvPath);
+
+            // Assert
+            Assert.Throws<WalletManagerDTO.Exceptions.SerializerException>(serializeAction);
+        }
+    }
+}
