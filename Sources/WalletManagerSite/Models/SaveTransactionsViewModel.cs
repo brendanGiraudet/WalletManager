3974cf16fb78c9b31cf040fb5ad9be88a08a8539
baka <bakayarusama@gmail.com>
2020-05-09 19:09:40 +0200

    update save csv file error

--- Sources/WalletManagerSite/Models/SaveTransactionsViewModel.cs
+++ Sources/WalletManagerSite/Models/SaveTransactionsViewModel.cs
@@ -6,7 +6,7 @@ namespace WalletManagerSite.Models
     public class SaveTransactionsViewModel
     {
         [Required(ErrorMessage = "Choose a filename of your csv file"),
-        FileExtensions(Extensions = "csv", ErrorMessage = "Error")]
+        FileExtensions(Extensions = "csv", ErrorMessage = "Use csv extension")]
         public string FileName { get; set; }
 
     }
