41e7b4f65505ae523b8dc49f7ba8ab7d602d8f8b
baka <bakayarusama@gmail.com>
2020-07-10 23:54:33 +0200

    fix mapper namespace

--- Sources/WalletManagerTestProject/MapperTests.cs
+++ Sources/WalletManagerTestProject/MapperTests.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using WalletManagerSite.Mapper;
+using WalletManagerSite.Tools.Mapper;
 using WalletManagerTestProject.Utils;
 using Xunit;
 
