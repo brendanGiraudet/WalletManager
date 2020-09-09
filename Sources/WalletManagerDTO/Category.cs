2e1427f7e01445e9455b1ac6f128d6e1edac7991
baka <bakayarusama@gmail.com>
2020-09-09 23:11:37 +0200

    Add category dto

---
+++ Sources/WalletManagerDTO/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WalletManagerDTO
+{
+    public class Category
+    {
+        public string Name { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public override string ToString()
+        {
+            return $"Category Name : {Name}, Creation Date : {CreationDate}";
+
+        }
+    }
+}
