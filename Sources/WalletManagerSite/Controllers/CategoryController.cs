800d12d37a7e2ff82ae0cbe3b59d36aead8d983f
baka <bakayarusama@gmail.com>
2020-10-05 19:39:13 +0200

    List categories

---
+++ Sources/WalletManagerSite/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using WalletManagerDTO;
+using WalletManagerSite.Tools.Mapper;
+using WalletManagerServices.Transaction;
+using WalletManagerSite.Models;
+using WalletManagerServices.Category;
+
+namespace WalletManagerSite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        readonly ICategoryServices _categoryServices;
+        readonly IConfiguration _configuration;
+        readonly IMapper _mapper;
+
+        public CategoryController(
+            ICategoryServices categoryServices, 
+            IConfiguration configuration,
+            IMapper mapper)
+        {
+            _categoryServices = categoryServices;
+            _configuration = configuration;
+            _mapper = mapper;
+        }
+        // GET: Compare
+        public ActionResult Index()
+        {
+            var categories = GetCategories();
+            var categoryViewModels = GetCategoryViewModels(categories);
+            return View(categoryViewModels);
+        }
+
+        private IEnumerable<Category> GetCategories()
+        {
+            var filePath = Tools.Directory.DirectoryTools.GetCategoryCsvFilePath(_configuration);
+            var categories = _categoryServices.GetCategories(filePath);
+            return categories;
+        }
+
+        private IEnumerable<CategoryViewModel> GetCategoryViewModels(IEnumerable<Category> categories) => categories.Select(_mapper.MapToCategoryViewModel);
+    }
+}
---
+++ Sources/WalletManagerSite/Models/CategoryViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WalletManagerSite.Models
+{
+    public class CategoryViewModel
+    {
+        public string CategoryName { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
