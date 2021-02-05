d60a5a761a6cd526ec064586f38f518182fafa7d
baka <bakayarusama@gmail.com>
2021-02-05 20:11:28 +0100

    fix response property

--- Sources/WalletManagerDTO/Response.cs
+++ Sources/WalletManagerDTO/Response.cs
@@ -2,7 +2,7 @@
 {
     public class Response<T>
     {
-        public bool HasError => string.IsNullOrEmpty(ErrorsMessage);
+        public bool HasError => !string.IsNullOrEmpty(ErrorsMessage);
         public string ErrorsMessage { get; set; }
         public T Content { get; set; }
     }
