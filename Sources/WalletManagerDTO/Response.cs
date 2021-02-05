568f95eb6a59f5f4f638004d4b3de2d753199c36
baka <bakayarusama@gmail.com>
2021-02-05 20:03:03 +0100

    add Response method dto

---
+++ Sources/WalletManagerDTO/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace WalletManagerDTO
+{
+    public class Response<T>
+    {
+        public bool HasError => string.IsNullOrEmpty(ErrorsMessage);
+        public string ErrorsMessage { get; set; }
+        public T Content { get; set; }
+    }
+}
