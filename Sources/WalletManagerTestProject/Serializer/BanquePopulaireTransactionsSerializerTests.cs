bc54d8c7724a525c2bc578e4909a8837a8d91ef9
baka <bakayarusama@gmail.com>
2021-02-05 22:19:15 +0100

    fix TU for CICD

--- Sources/WalletManagerTestProject/Serializer/BanquePopulaireTransactionsSerializerTests.cs
+++ Sources/WalletManagerTestProject/Serializer/BanquePopulaireTransactionsSerializerTests.cs
@@ -16,8 +16,8 @@ namespace WalletManagerTestProject.Serializer
         {
             _serializer = new BanquePopulaireTransactionsSerializer();
             _contentFile = _contentFile.Append("Compte;Date de comptabilisation;Date opération;Libellé;Référence;Date valeur;Montant");
-            _contentFile = _contentFile.Append("31419185918;31-08-2020;31/08/2020;VIR M BRENDAN GIRAUDET Virement vers BRENDAN GIRAUDET;9896465;31/08/2020;-600,00");
-            _contentFile = _contentFile.Append("31419185918;31-08-2020;31/08/2020;290820 CB****1526 HALLES COIGNIER78COIGNIERES; B870NZD;31/08/2020;-47,07;");
+            _contentFile = _contentFile.Append("31419185918;16-Jun-20 12:36:14 AM;16-Jun-20 12:36:14 AM;VIR M BRENDAN GIRAUDET Virement vers BRENDAN GIRAUDET;9896465;31/08/2020;-600,00");
+            _contentFile = _contentFile.Append("31419185918;16-Jun-20 12:36:14 AM;16-Jun-20 12:36:14 AM;290820 CB****1526 HALLES COIGNIER78COIGNIERES; B870NZD;31/08/2020;-47,07;");
         }
 
         #region Deserialize
