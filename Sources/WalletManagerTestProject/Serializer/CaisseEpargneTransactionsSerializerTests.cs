769b5adc9213bdc864e8c79e28b46513976b6cdf
baka <bakayarusama@gmail.com>
2021-02-05 19:30:13 +0100

    use contentfile instead real file

--- Sources/WalletManagerTestProject/Serializer/CaisseEpargneTransactionsSerializerTests.cs
+++ Sources/WalletManagerTestProject/Serializer/CaisseEpargneTransactionsSerializerTests.cs
@@ -3,19 +3,26 @@ using Xunit;
 using System.IO;
 using System.Linq;
 using WalletManagerDTO;
+using System.Collections.Generic;
 
 namespace WalletManagerTestProject.Serializer
 {
     public class CaisseEpargneTransactionsSerializerTests
     {
-        const string csvBasePath = @"/home/runner/work/WalletManager/WalletManager/Sources/WalletManagerTestProject/CaisseEpargneTransactionsSerializerCsv/";
-        //const string csvBasePath = @"D:\document\project\WalletManager\Sources\WalletManagerTestProject\CaisseEpargneTransactionsSerializerCsv\";
+        private IEnumerable<string> _contentFile = Enumerable.Empty<string>();
 
         readonly ISerializer<Transaction> _serializer;
 
         public CaisseEpargneTransactionsSerializerTests()
         {
             _serializer = new CaisseEpargneTransactionsSerializer();
+            _contentFile = _contentFile.Append("Code de la banque : 14505;Code de l'agence : 00001;Date de début de téléchargement : 01/10/2020;Date de fin de téléchargement : 31/10/2020;");
+            _contentFile = _contentFile.Append("Numéro de compte : 04309621484;Nom du compte : COMPTE CHEQUE;Devise : EUR;");
+            _contentFile = _contentFile.Append("");
+            _contentFile = _contentFile.Append("Solde en fin de période;;;;3480,82");
+            _contentFile = _contentFile.Append("Date;Numéro d'opération;Libellé;Débit;Crédit;Détail;");
+            _contentFile = _contentFile.Append("28/10/20;2810202020201028-17.58.04.478319 -;CB L ORIENT PALACE FACT 271020;-12,10;;CB L ORIENT PALACE FACT 271020 ;");
+            _contentFile = _contentFile.Append("27/10/20;2710202020201027-09.25.47.953651 -;CB INTERMARCHE     FACT 261020;-96,31;;CB INTERMARCHE     FACT 261020 ;");
         }
 
         #region Deserialize
@@ -23,11 +30,9 @@ namespace WalletManagerTestProject.Serializer
         public void ShouldDeserializeListOfTransaction()
         {
             // Arrange
-            var csvFilePath = Path.Combine(csvBasePath, "deserializeTransactions.csv");
-            var lines = File.ReadAllLines(csvFilePath);
 
             // Act
-            var transactions = _serializer.Deserialize(lines);
+            var transactions = _serializer.Deserialize(_contentFile);
 
             // Assert
             Assert.True(transactions.Any());
